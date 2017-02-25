ceed557eb464afa348f7e207492810f1e2be90f4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:42:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Upplýsingar um skipulag er notað til að búa til öryggisvottanir og önnur vanskil hugbúnaður.';
+$lang['organization_app_name'] = 'skipulag';
+$lang['organization_city'] = 'Borg';
+$lang['organization_city_invalid'] = 'City er ógilt.';
+$lang['organization_country'] = 'Land';
+$lang['organization_country_invalid'] = 'Country er ógilt.';
+$lang['organization_organization_invalid'] = 'Organization er ógilt.';
+$lang['organization_organization'] = 'skipulag';
+$lang['organization_postal_code_invalid'] = 'Póstnúmer er ógildur.';
+$lang['organization_postal_code'] = 'Postal / Zip Code';
+$lang['organization_region_invalid'] = 'State, Province eða Region er ógilt.';
+$lang['organization_region'] = 'Ríki, fylki eða svæði';
+$lang['organization_street_address_invalid'] = 'Heimilisfang er ógilt.';
+$lang['organization_street_address'] = 'Heimilisfang';
+$lang['organization_unit_invalid'] = 'Eining er ógilt.';
+$lang['organization_unit'] = 'Unit';
