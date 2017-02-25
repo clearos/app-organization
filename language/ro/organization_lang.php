252f3594882d0e858be09ec8620fe5c7c3800c97
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:45:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Informații despre organizația dumneavoastră este utilizată pentru a genera certificate de securitate și alte valori implicite de software.';
+$lang['organization_app_name'] = 'Organizare';
+$lang['organization_city'] = 'Oraș';
+$lang['organization_city_invalid'] = 'Orașul este nevalid.';
+$lang['organization_country'] = 'Țară';
+$lang['organization_country_invalid'] = 'Țara este nevalid.';
+$lang['organization_organization_invalid'] = 'Organizație este nevalid.';
+$lang['organization_organization'] = 'Organizare';
+$lang['organization_postal_code_invalid'] = 'Codul poștal este nevalid.';
+$lang['organization_postal_code'] = 'Cod poștal';
+$lang['organization_region_invalid'] = 'Stat, provincie sau regiune nu este validă.';
+$lang['organization_region'] = 'Stat, Provincie sau Regiune';
+$lang['organization_street_address_invalid'] = 'adresă nu este validă.';
+$lang['organization_street_address'] = 'Adresa străzii';
+$lang['organization_unit_invalid'] = 'Unitate este nevalid.';
+$lang['organization_unit'] = 'Unitate';
