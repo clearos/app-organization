31bca8bd18fc9555cff315a1cb7450700b1dcb9e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:43:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = '조직에 대한 정보는 보안 인증서 및 기타 소프트웨어 기본값을 생성하는 데 사용됩니다.';
+$lang['organization_app_name'] = '조직';
+$lang['organization_city'] = '시티';
+$lang['organization_city_invalid'] = '도시가 잘못되었습니다.';
+$lang['organization_country'] = '국가';
+$lang['organization_country_invalid'] = '나라가 잘못되었습니다.';
+$lang['organization_organization_invalid'] = '조직이 잘못되었습니다.';
+$lang['organization_organization'] = '조직';
+$lang['organization_postal_code_invalid'] = '우편 번호가 잘못되었습니다.';
+$lang['organization_postal_code'] = '우편 / 우편 번호';
+$lang['organization_region_invalid'] = '국가, 지방 또는 지역이 잘못되었습니다.';
+$lang['organization_region'] = '국가, 지방 또는 지역';
+$lang['organization_street_address_invalid'] = '주소가 잘못되었습니다.';
+$lang['organization_street_address'] = '번지';
+$lang['organization_unit_invalid'] = '단위가 잘못되었습니다.';
+$lang['organization_unit'] = '단위';
