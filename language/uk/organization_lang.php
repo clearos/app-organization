9ceb89d8a5d53cded4be82d77b2dbf5ed40a0549
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:48:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Інформація про вашу організацію використовується для створення сертифікатів безпеки та інше програмне забезпечення за замовчуванням.';
+$lang['organization_app_name'] = 'організація';
+$lang['organization_city'] = 'місто';
+$lang['organization_city_invalid'] = 'Місто є недійсним.';
+$lang['organization_country'] = 'країна';
+$lang['organization_country_invalid'] = 'Країна є недійсним.';
+$lang['organization_organization_invalid'] = 'Організація є недійсним.';
+$lang['organization_organization'] = 'організація';
+$lang['organization_postal_code_invalid'] = 'Поштовий індекс є недійсним.';
+$lang['organization_postal_code'] = 'Поштовий / Поштовий індекс';
+$lang['organization_region_invalid'] = 'Штат, провінція чи регіон є недійсним.';
+$lang['organization_region'] = 'Штат, провінція чи регіон';
+$lang['organization_street_address_invalid'] = 'Вулиця адресу недійсний.';
+$lang['organization_street_address'] = 'Адреса вулиці';
+$lang['organization_unit_invalid'] = 'Одиниця є недійсним.';
+$lang['organization_unit'] = 'блок';
