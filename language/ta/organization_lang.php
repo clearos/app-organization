460df7cd34b027011807482b8256464f90dddbba
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:47:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'உங்கள் அமைப்பு பற்றி தகவல் பாதுகாப்பு சான்றிதழ்கள் மற்றும் பிற மென்பொருள் இயல்புநிலைக்கு உருவாக்க பயன்படுத்தப்படுகிறது.';
+$lang['organization_app_name'] = 'அமைப்பு';
+$lang['organization_city'] = 'சிட்டி';
+$lang['organization_city_invalid'] = 'நகரம் தவறானது.';
+$lang['organization_country'] = 'நாடு';
+$lang['organization_country_invalid'] = 'நாடு தவறானது.';
+$lang['organization_organization_invalid'] = 'அமைப்பு தவறானது.';
+$lang['organization_organization'] = 'அமைப்பு';
+$lang['organization_postal_code_invalid'] = 'அஞ்சல் குறியீடு தவறானது.';
+$lang['organization_postal_code'] = 'அஞ்சல் / ஜிப் குறியீடு';
+$lang['organization_region_invalid'] = 'மாநில, மாகாண அல்லது மண்டலம் தவறானது.';
+$lang['organization_region'] = 'மாநில, மாகாண அல்லது பகுதி';
+$lang['organization_street_address_invalid'] = 'தெரு முகவரி தவறானது.';
+$lang['organization_street_address'] = 'தெரு முகவரி';
+$lang['organization_unit_invalid'] = 'அலகு தவறானது.';
+$lang['organization_unit'] = 'அலகு';
