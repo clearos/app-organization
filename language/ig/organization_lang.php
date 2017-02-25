e967e27224e4de875a47e3d8f6d40a015773eca5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:41:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Ozi gbasara gị nzukọ na-eji na-n&#39;ịwa nche asambodo na ndị ọzọ na software defaults.';
+$lang['organization_app_name'] = 'Organization';
+$lang['organization_city'] = 'City';
+$lang['organization_city_invalid'] = 'City bụ ghara ịdị irè.';
+$lang['organization_country'] = 'Country';
+$lang['organization_country_invalid'] = 'Country bụ ghara ịdị irè.';
+$lang['organization_organization_invalid'] = 'Organization bụ ghara ịdị irè.';
+$lang['organization_organization'] = 'Organization';
+$lang['organization_postal_code_invalid'] = 'Koodu nzi ozi bụ ghara ịdị irè.';
+$lang['organization_postal_code'] = 'Postal / Zip Code';
+$lang['organization_region_invalid'] = 'State, Province ma ọ bụ Region bụ ghara ịdị irè.';
+$lang['organization_region'] = 'State, Province ma ọ bụ Region';
+$lang['organization_street_address_invalid'] = 'Street adreesị bụ ghara ịdị irè.';
+$lang['organization_street_address'] = 'Adreesị Striit';
+$lang['organization_unit_invalid'] = 'Unit bụ ghara ịdị irè.';
+$lang['organization_unit'] = 'unit';
