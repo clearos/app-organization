76aea2221d8456c3c89a79aa679978b213d379d6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:43:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Informācija par jūsu organizāciju tiek izmantota, lai radītu drošības sertifikāti un citas programmatūras noklusējumus.';
+$lang['organization_app_name'] = 'organizēšana';
+$lang['organization_city'] = 'pilsēta';
+$lang['organization_city_invalid'] = 'Pilsēta ir nederīgs.';
+$lang['organization_country'] = 'valsts';
+$lang['organization_country_invalid'] = 'Valsts ir nederīgs.';
+$lang['organization_organization_invalid'] = 'Organizācija ir nederīgs.';
+$lang['organization_organization'] = 'organizēšana';
+$lang['organization_postal_code_invalid'] = 'Pasta kods ir nederīgs.';
+$lang['organization_postal_code'] = 'Pasta / Zip Code';
+$lang['organization_region_invalid'] = 'Valsts, Province vai reģions ir nederīgs.';
+$lang['organization_region'] = 'Valsts, Province vai reģions';
+$lang['organization_street_address_invalid'] = 'Adrese ir nederīga.';
+$lang['organization_street_address'] = 'Ielas adrese';
+$lang['organization_unit_invalid'] = 'Vienība ir nederīgs.';
+$lang['organization_unit'] = 'vienība';
