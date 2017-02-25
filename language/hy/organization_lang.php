f876e76893a0dd57c961bb67690a95f1bab00975
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:38:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Ձեր կազմակերպության մասին, որն օգտագործվում է առաջացնում անվտանգության վկայագրեր եւ այլ ծրագրային ապահովման Նախնականը.';
+$lang['organization_app_name'] = 'կազմակերպություն';
+$lang['organization_city'] = 'քաղաք';
+$lang['organization_city_invalid'] = 'City անվավեր է.';
+$lang['organization_country'] = 'երկիր';
+$lang['organization_country_invalid'] = 'Երկիր անվավեր է:';
+$lang['organization_organization_invalid'] = 'Կազմակերպությունը անվավեր է:';
+$lang['organization_organization'] = 'կազմակերպություն';
+$lang['organization_postal_code_invalid'] = 'Փոստային կոդը անվավեր է:';
+$lang['organization_postal_code'] = 'Փոստային / Zip Code';
+$lang['organization_region_invalid'] = 'Պետական, մարզ կամ շրջան անվավեր է:';
+$lang['organization_region'] = 'Պետական, մարզ կամ շրջան';
+$lang['organization_street_address_invalid'] = 'Street հասցեն անվավեր է:';
+$lang['organization_street_address'] = 'փողոց Հասցե';
+$lang['organization_unit_invalid'] = 'Unit է անվավեր:';
+$lang['organization_unit'] = 'միավոր';
