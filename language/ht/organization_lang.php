aeb08642c027f056a96207b5b230fc4ca062919f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:41:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Enfòmasyon sou òganizasyon ou se sèvi ak jenere sètifika sekirite ak lòt défaut lojisyèl.';
+$lang['organization_app_name'] = 'Òganizasyon';
+$lang['organization_city'] = 'City';
+$lang['organization_city_invalid'] = 'City se valab.';
+$lang['organization_country'] = 'peyi';
+$lang['organization_country_invalid'] = 'Peyi se valab.';
+$lang['organization_organization_invalid'] = 'Òganizasyon se valab.';
+$lang['organization_organization'] = 'Òganizasyon';
+$lang['organization_postal_code_invalid'] = 'Sèvis kòd se valab.';
+$lang['organization_postal_code'] = 'Sèvis / Kòd Postal';
+$lang['organization_region_invalid'] = 'Eta a, Pwovens oswa Rejyon se valab.';
+$lang['organization_region'] = 'Eta a, Pwovens oswa Rejyon';
+$lang['organization_street_address_invalid'] = 'adrès Street se valab.';
+$lang['organization_street_address'] = 'Adrès Ri';
+$lang['organization_unit_invalid'] = 'Inite se valab.';
+$lang['organization_unit'] = 'inite';
