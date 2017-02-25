81a21f332eef25514a6af3289781c04aa7f09297
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:45:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Информация о вашей организации используется для создания сертификатов безопасности и другое программное обеспечение по умолчанию.';
+$lang['organization_app_name'] = 'организация';
+$lang['organization_city'] = 'город';
+$lang['organization_city_invalid'] = 'Город является недействительным.';
+$lang['organization_country'] = 'Страна';
+$lang['organization_country_invalid'] = 'Страна является недействительным.';
+$lang['organization_organization_invalid'] = 'Организация является недействительным.';
+$lang['organization_organization'] = 'организация';
+$lang['organization_postal_code_invalid'] = 'Почтовый индекс является недействительным.';
+$lang['organization_postal_code'] = 'Почтовый / Почтовый индекс';
+$lang['organization_region_invalid'] = 'Штат, провинция или регион является недействительным.';
+$lang['organization_region'] = 'Штат, провинция, или регион';
+$lang['organization_street_address_invalid'] = 'Улица адрес недействителен.';
+$lang['organization_street_address'] = 'Адрес улицы';
+$lang['organization_unit_invalid'] = 'Единица является недействительным.';
+$lang['organization_unit'] = 'Ед. изм';
