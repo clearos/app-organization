01b3ece4c5d5fc8e7a700971d04a4bd3f5929e69
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:38:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Інфармацыя аб вашай арганізацыі выкарыстоўваецца для стварэння сертыфікатаў бяспекі і іншае праграмнае забеспячэнне па змаўчанні.';
+$lang['organization_app_name'] = 'арганізацыя';
+$lang['organization_city'] = 'горад';
+$lang['organization_city_invalid'] = 'Горад з&#39;яўляецца несапраўдным.';
+$lang['organization_country'] = 'краіна';
+$lang['organization_country_invalid'] = 'Краіна з&#39;яўляецца несапраўдным.';
+$lang['organization_organization_invalid'] = 'Арганізацыя з&#39;яўляецца несапраўдным.';
+$lang['organization_organization'] = 'арганізацыя';
+$lang['organization_postal_code_invalid'] = 'Паштовы індэкс з&#39;яўляецца несапраўдным.';
+$lang['organization_postal_code'] = 'Паштовы / паштовы індэкс';
+$lang['organization_region_invalid'] = 'Штат, правінцыя або рэгіён з&#39;яўляецца несапраўдным.';
+$lang['organization_region'] = 'Штат, правінцыя або рэгіён';
+$lang['organization_street_address_invalid'] = 'Вуліца адрас ня дзейнічае.';
+$lang['organization_street_address'] = 'вуліца';
+$lang['organization_unit_invalid'] = 'Адзінка з&#39;яўляецца несапраўдным.';
+$lang['organization_unit'] = 'блок';
