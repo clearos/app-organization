26cdb8713a6b7441a5e7c5631fa5ca0f2fc41e82
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:47:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'మీ సంస్థ గురించి సమాచార భద్రత సర్టిఫికేట్లు మరియు ఇతర సాఫ్ట్వేర్ అప్రమేయం సృష్టించేందుకు ఉపయోగిస్తారు.';
+$lang['organization_app_name'] = 'సంస్థ';
+$lang['organization_city'] = 'సిటీ';
+$lang['organization_city_invalid'] = 'సిటీ చెల్లదు.';
+$lang['organization_country'] = 'దేశం';
+$lang['organization_country_invalid'] = 'దేశం చెల్లనిది.';
+$lang['organization_organization_invalid'] = 'సంస్థ చెల్లదు.';
+$lang['organization_organization'] = 'సంస్థ';
+$lang['organization_postal_code_invalid'] = 'పోస్టల్ కోడ్ చెల్లదు.';
+$lang['organization_postal_code'] = 'పోస్టల్ / జిప్ కోడ్';
+$lang['organization_region_invalid'] = 'రాష్ట్రం, ప్రాంతం లేదా ప్రాంతం చెల్లదు.';
+$lang['organization_region'] = 'రాష్ట్రం, ప్రాంతం లేదా ప్రాంతం';
+$lang['organization_street_address_invalid'] = 'వీధి చిరునామా చెల్లనిది.';
+$lang['organization_street_address'] = 'చిరునామా';
+$lang['organization_unit_invalid'] = 'యూనిట్ చెల్లదు.';
+$lang['organization_unit'] = 'యూనిట్';
