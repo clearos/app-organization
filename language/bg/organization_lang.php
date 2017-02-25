3bbe5ad2f4b4175ff7b754ca93da4b5a534d7e57
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:39:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Информация за организацията се използва за генериране на сертификати за сигурност и други софтуерни настройки.';
+$lang['organization_app_name'] = 'организация';
+$lang['organization_city'] = 'град';
+$lang['organization_city_invalid'] = 'Сити е невалиден.';
+$lang['organization_country'] = 'Държава';
+$lang['organization_country_invalid'] = 'Държава е невалиден.';
+$lang['organization_organization_invalid'] = 'Организация е невалиден.';
+$lang['organization_organization'] = 'организация';
+$lang['organization_postal_code_invalid'] = 'Пощенски код е невалиден.';
+$lang['organization_postal_code'] = 'Пощенски / Zip Code';
+$lang['organization_region_invalid'] = 'Членка, провинция или регион е невалиден.';
+$lang['organization_region'] = 'Членка, провинция или регион';
+$lang['organization_street_address_invalid'] = 'Улица адрес е невалиден.';
+$lang['organization_street_address'] = 'Адрес на улица';
+$lang['organization_unit_invalid'] = 'Unit е невалиден.';
+$lang['organization_unit'] = 'Мерна единица';
