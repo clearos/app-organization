7a2755c09524adcfd0b1f80e5ee75cf7b46c025c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:46:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Podatki o vaši organizaciji se uporablja za ustvarjanje varnostne certifikate in druge programske privzete.';
+$lang['organization_app_name'] = 'organizacija';
+$lang['organization_city'] = 'mesto';
+$lang['organization_city_invalid'] = 'City je neveljavna.';
+$lang['organization_country'] = 'Država';
+$lang['organization_country_invalid'] = 'Država je neveljavna.';
+$lang['organization_organization_invalid'] = 'Organizacija je neveljavna.';
+$lang['organization_organization'] = 'organizacija';
+$lang['organization_postal_code_invalid'] = 'Poštna koda je neveljavna.';
+$lang['organization_postal_code'] = 'Postal / Poštna številka';
+$lang['organization_region_invalid'] = 'Država, pokrajina ali regija je neveljavna.';
+$lang['organization_region'] = 'Država, pokrajina ali regija';
+$lang['organization_street_address_invalid'] = 'Ulica in hišna številka ni veljavna.';
+$lang['organization_street_address'] = 'Naslov ceste';
+$lang['organization_unit_invalid'] = 'Enota je neveljavna.';
+$lang['organization_unit'] = 'enota';
