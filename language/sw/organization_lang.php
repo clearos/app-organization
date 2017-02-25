c2be0bc89f878e8ba391412910f701a774aaa7e1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:47:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Habari kuhusu asasi yako ni kutumika kuzalisha vyeti vya usalama na defaults programu nyingine.';
+$lang['organization_app_name'] = 'Shirika';
+$lang['organization_city'] = 'City';
+$lang['organization_city_invalid'] = 'City ni batili.';
+$lang['organization_country'] = 'Nchi';
+$lang['organization_country_invalid'] = 'Nchi ni batili.';
+$lang['organization_organization_invalid'] = 'Shirika ni batili.';
+$lang['organization_organization'] = 'Shirika';
+$lang['organization_postal_code_invalid'] = 'Code posta ni batili.';
+$lang['organization_postal_code'] = 'Postal / Zip Code';
+$lang['organization_region_invalid'] = 'Jimbo, Mkoa wa au Eneo ni batili.';
+$lang['organization_region'] = 'Jimbo, Mkoa wa au Eneo';
+$lang['organization_street_address_invalid'] = 'Anwani ya mitaani ni batili.';
+$lang['organization_street_address'] = 'Anuani ya mtaa';
+$lang['organization_unit_invalid'] = 'Unit ni batili.';
+$lang['organization_unit'] = 'Unit';
