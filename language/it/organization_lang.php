6587dae966d4c70a017e417b1cf13d3ac345ae30
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:42:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/it/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Informazioni sulla vostra organizzazione è utilizzato per generare certificati di protezione e altre impostazioni predefinite del software.';
+$lang['organization_app_name'] = 'Organizzazione';
+$lang['organization_city'] = 'Città';
+$lang['organization_city_invalid'] = 'Città non è valido.';
+$lang['organization_country'] = 'Nazione';
+$lang['organization_country_invalid'] = 'Paese non è valido.';
+$lang['organization_organization_invalid'] = 'L&#39;organizzazione è valido.';
+$lang['organization_organization'] = 'Organizzazione';
+$lang['organization_postal_code_invalid'] = 'Codice postale non è valido.';
+$lang['organization_postal_code'] = 'Codice postale';
+$lang['organization_region_invalid'] = 'Stato, Provincia o Regione non è valido.';
+$lang['organization_region'] = 'stato, Provincia o regione';
+$lang['organization_street_address_invalid'] = 'indirizzo non è valido.';
+$lang['organization_street_address'] = 'Indirizzo';
+$lang['organization_unit_invalid'] = 'Unità non è valido.';
+$lang['organization_unit'] = 'Unità';
