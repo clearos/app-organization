8775e04ec3a5e1e81787766aebfacc974d23f4d5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:43:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'About Us securitatem generare consuevit et software vestra organizationem defectus.';
+$lang['organization_app_name'] = 'organization';
+$lang['organization_city'] = 'Urbs';
+$lang['organization_city_invalid'] = 'Est invalidum.';
+$lang['organization_country'] = 'Patriam';
+$lang['organization_country_invalid'] = 'Patria est invalidum.';
+$lang['organization_organization_invalid'] = 'Organization est invalidum.';
+$lang['organization_organization'] = 'organization';
+$lang['organization_postal_code_invalid'] = 'Curabitur dolor sit irrita.';
+$lang['organization_postal_code'] = 'Curabitur / Pellentesque';
+$lang['organization_region_invalid'] = 'Romanam provinciam vel regionem nulla.';
+$lang['organization_region'] = 'State, provinciam vel regionem';
+$lang['organization_street_address_invalid'] = 'Street est oratio nulla.';
+$lang['organization_street_address'] = 'Locus viae';
+$lang['organization_unit_invalid'] = 'Unit est invalidum.';
+$lang['organization_unit'] = 'Unit';
