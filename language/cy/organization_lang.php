31e5f45c5d0d8f97f288ee68f8f3aa4a85a180f4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:48:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Gwybodaeth am eich mudiad yn cael ei ddefnyddio i gynhyrchu tystysgrifau diogelwch a diffygion meddalwedd eraill.';
+$lang['organization_app_name'] = 'sefydliad';
+$lang['organization_city'] = 'City';
+$lang['organization_city_invalid'] = 'Dinas yn annilys.';
+$lang['organization_country'] = 'gwlad';
+$lang['organization_country_invalid'] = 'Gwlad yn annilys.';
+$lang['organization_organization_invalid'] = 'Sefydliad yn annilys.';
+$lang['organization_organization'] = 'sefydliad';
+$lang['organization_postal_code_invalid'] = 'cod post yn annilys.';
+$lang['organization_postal_code'] = 'Cod Post / Zip';
+$lang['organization_region_invalid'] = 'Wladwriaeth, Talaith neu Ranbarth yn annilys.';
+$lang['organization_region'] = 'Wladwriaeth, Talaith neu Ranbarth';
+$lang['organization_street_address_invalid'] = 'cyfeiriad stryd yn annilys.';
+$lang['organization_street_address'] = 'Cyfeiriad stryd';
+$lang['organization_unit_invalid'] = 'Uned yn annilys.';
+$lang['organization_unit'] = 'Uned';
