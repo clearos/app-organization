ef9fd7e5377823547303a52b21e206206d2fcfd0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:42:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Ұйымыңыздағы туралы ақпарат қауіпсіздік сертификаттар мен басқа да бағдарламалық қамтамасыз ету əдепкі параметрлерін жасау үшін пайдаланылады.';
+$lang['organization_app_name'] = 'ұйым';
+$lang['organization_city'] = 'қала';
+$lang['organization_city_invalid'] = 'Қала жарамсыз болып табылады.';
+$lang['organization_country'] = 'ел';
+$lang['organization_country_invalid'] = 'Country жарамсыз болып табылады.';
+$lang['organization_organization_invalid'] = 'Ұйым жарамсыз болып табылады.';
+$lang['organization_organization'] = 'ұйым';
+$lang['organization_postal_code_invalid'] = 'Пошта индексі жарамсыз болып табылады.';
+$lang['organization_postal_code'] = 'Пошта / Индекс';
+$lang['organization_region_invalid'] = 'Мемлекеттік, Province немесе аймақ жарамсыз.';
+$lang['organization_region'] = 'Мемлекеттік, Province немесе аймақ';
+$lang['organization_street_address_invalid'] = 'Көше мекенжайы жарамсыз болып табылады.';
+$lang['organization_street_address'] = 'Көше адресі';
+$lang['organization_unit_invalid'] = 'Unit жарамсыз болып табылады.';
+$lang['organization_unit'] = 'бірлік';
