e88e9842ba4345b478c219c8a473169735d6d641
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:42:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'ನಿಮ್ಮ ಸಂಸ್ಥೆಯ ಬಗ್ಗೆ ಮಾಹಿತಿಯ ಸುರಕ್ಷತೆ ಪ್ರಮಾಣಪತ್ರಗಳನ್ನು ಮತ್ತು ಇತರ ತಂತ್ರಾಂಶ ಡಿಫಾಲ್ಟ್ ಸೃಷ್ಟಿಸಲು ಬಳಸಲಾಗುತ್ತದೆ.';
+$lang['organization_app_name'] = 'ಸಂಸ್ಥೆ';
+$lang['organization_city'] = 'ಸಿಟಿ';
+$lang['organization_city_invalid'] = 'ಸಿಟಿ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['organization_country'] = 'ದೇಶದ';
+$lang['organization_country_invalid'] = 'ರಾಷ್ಟ್ರ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['organization_organization_invalid'] = 'ಸಂಸ್ಥೆ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['organization_organization'] = 'ಸಂಸ್ಥೆ';
+$lang['organization_postal_code_invalid'] = 'ಪೋಸ್ಟಲ್ ಕೋಡ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['organization_postal_code'] = 'ಅಂಚೆ / ಪಿನ್ ಕೋಡ್';
+$lang['organization_region_invalid'] = 'ರಾಜ್ಯ, ಪ್ರಾಂತ್ಯ ಅಥವಾ ಪ್ರದೇಶ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['organization_region'] = 'ರಾಜ್ಯ, ಪ್ರಾಂತ್ಯ ಅಥವಾ ಪ್ರದೇಶ';
+$lang['organization_street_address_invalid'] = 'ರಸ್ತೆಯಲ್ಲಿ ವಿಳಾಸ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['organization_street_address'] = 'ರಸ್ತೆ ವಿಳಾಸ';
+$lang['organization_unit_invalid'] = 'ಘಟಕ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['organization_unit'] = 'ಘಟಕ';
