6993e30a03acb99d0a3b8fff0aaaf455c34f10fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:45:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'اطلاعات در مورد سازمان شما مورد استفاده برای تولید گواهینامه های امنیتی و دیگر پیش فرض نرم افزار است.';
+$lang['organization_app_name'] = 'سازمان';
+$lang['organization_city'] = 'شهرستان';
+$lang['organization_city_invalid'] = 'شهرستان نامعتبر است.';
+$lang['organization_country'] = 'کشور';
+$lang['organization_country_invalid'] = 'کشور نامعتبر است.';
+$lang['organization_organization_invalid'] = 'سازمان نامعتبر است.';
+$lang['organization_organization'] = 'سازمان';
+$lang['organization_postal_code_invalid'] = 'کد پستی نامعتبر است.';
+$lang['organization_postal_code'] = 'پستی / کد پستی';
+$lang['organization_region_invalid'] = 'دولت، استان یا منطقه نامعتبر است.';
+$lang['organization_region'] = 'دولت، استان یا منطقه';
+$lang['organization_street_address_invalid'] = 'آدرس خیابان نامعتبر است.';
+$lang['organization_street_address'] = 'آدرس خیابان';
+$lang['organization_unit_invalid'] = 'واحد نامعتبر است.';
+$lang['organization_unit'] = 'واحد';
