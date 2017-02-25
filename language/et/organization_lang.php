8c4578424d8d12e8134fe3a4497f2aff45e77fe3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:40:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Teave teie organisatsiooni kasutatakse, et luua turvalisuse tunnistus ja muud tarkvara vaikimisi.';
+$lang['organization_app_name'] = 'organisatsioon';
+$lang['organization_city'] = 'linn';
+$lang['organization_city_invalid'] = 'City on kehtetu.';
+$lang['organization_country'] = 'Riik';
+$lang['organization_country_invalid'] = 'Riik on kehtetu.';
+$lang['organization_organization_invalid'] = 'Organisatsioon on kehtetu.';
+$lang['organization_organization'] = 'organisatsioon';
+$lang['organization_postal_code_invalid'] = 'Posti kood ei sobi.';
+$lang['organization_postal_code'] = 'Postiteenus / Postiindeks';
+$lang['organization_region_invalid'] = 'Riigi, provintsi või piirkond ei sobi.';
+$lang['organization_region'] = 'Riigi, provintsi või regioon';
+$lang['organization_street_address_invalid'] = 'Tänav on kehtetu.';
+$lang['organization_street_address'] = 'Tänava aadress';
+$lang['organization_unit_invalid'] = 'Unit on kehtetu.';
+$lang['organization_unit'] = 'üksus';
