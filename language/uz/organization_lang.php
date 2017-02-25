7f088d43af085ba2cbbb02ec1254fc0f8b159dc5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:48:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'tashkilotingiz haqida ma&#39;lumot xavfsizlik sertifikatlari va boshqa dasturlar standartni ishlab chiqarish uchun ishlatiladi.';
+$lang['organization_app_name'] = 'tashkilot';
+$lang['organization_city'] = 'shahar';
+$lang['organization_city_invalid'] = 'Shahar haqiqiy emas.';
+$lang['organization_country'] = 'mamlakat';
+$lang['organization_country_invalid'] = 'Mamlakat haqiqiy emas.';
+$lang['organization_organization_invalid'] = 'Tashkilot haqiqiy emas.';
+$lang['organization_organization'] = 'tashkilot';
+$lang['organization_postal_code_invalid'] = 'Pochta kodi noto&#39;g&#39;ri hisoblanadi.';
+$lang['organization_postal_code'] = 'Pochta / Zip Code';
+$lang['organization_region_invalid'] = 'Davlat Hudud haqiqiy emas.';
+$lang['organization_region'] = 'Davlat Hudud';
+$lang['organization_street_address_invalid'] = 'Street manzil noto&#39;g&#39;ri.';
+$lang['organization_street_address'] = 'Ko&#39;cha manzili';
+$lang['organization_unit_invalid'] = 'Birligi haqiqiy emas.';
+$lang['organization_unit'] = 'birlik';
