775222834c4675f5d5ff3be63956c10ab803bce5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:39:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Information za bungwe lanu ntchito kupanga zikalata chitetezo ndi defaults ena mapulogalamu.';
+$lang['organization_app_name'] = 'Organization';
+$lang['organization_city'] = 'City';
+$lang['organization_city_invalid'] = 'City ndi opanda pake.';
+$lang['organization_country'] = 'Country';
+$lang['organization_country_invalid'] = 'Dziko ili udzakhala osazindikilika.';
+$lang['organization_organization_invalid'] = 'Organization ndi opanda pake.';
+$lang['organization_organization'] = 'Organization';
+$lang['organization_postal_code_invalid'] = 'Keyala ndi opanda pake.';
+$lang['organization_postal_code'] = 'Positi / Zip';
+$lang['organization_region_invalid'] = 'State, Province kapena Region ndi opanda pake.';
+$lang['organization_region'] = 'State, Province kapena Region';
+$lang['organization_street_address_invalid'] = 'Adilesi ili udzakhala osazindikilika.';
+$lang['organization_street_address'] = 'Adilesi yamsewu';
+$lang['organization_unit_invalid'] = 'Unit ndi opanda pake.';
+$lang['organization_unit'] = 'Unit';
