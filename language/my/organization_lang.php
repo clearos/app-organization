8d86fedb6b15707d37df86fc3ffa9a4b70f18fed
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:39:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'သင့်အဖွဲ့အစည်းအကြောင်းကိုသတင်းအချက်အလက်လုံခြုံရေးလက်မှတ်များနှင့်တခြားဆော့ဗ်ဝဲရဲ့ default တန်ဖိုးထုတ်လုပ်ဖို့အသုံးပြုသည်။';
+$lang['organization_app_name'] = 'အဖှဲ့အစညျး';
+$lang['organization_city'] = 'မြို့';
+$lang['organization_city_invalid'] = 'စီးတီးဟာမမှန်ကန်ဘူး။';
+$lang['organization_country'] = 'ပြည်';
+$lang['organization_country_invalid'] = 'နိုင်ငံမမှန်ကန်ဘူး။';
+$lang['organization_organization_invalid'] = 'အဖှဲ့အစညျးမမှန်ကန်ဘူး။';
+$lang['organization_organization'] = 'အဖှဲ့အစညျး';
+$lang['organization_postal_code_invalid'] = 'စာတိုက် code ကိုမမှန်ကန်ဘူး။';
+$lang['organization_postal_code'] = 'စာတိုက် / Zip Code ကို';
+$lang['organization_region_invalid'] = 'ပြည်နယ်, ခရိုင်သို့မဟုတ်တိုင်းဒေသကြီးမမှန်ကန်ဘူး။';
+$lang['organization_region'] = 'ပြည်နယ်, ခရိုင်သို့မဟုတ်တိုင်းဒေသကြီး';
+$lang['organization_street_address_invalid'] = 'လမ်းလိပ်စာမမှန်ကန်ဘူး။';
+$lang['organization_street_address'] = 'လမ်းလိပ်စာ';
+$lang['organization_unit_invalid'] = 'ယူနစ်မမှန်ကန်ဘူး။';
+$lang['organization_unit'] = 'ယူနစ်';
