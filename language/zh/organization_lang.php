fe6b829514c9d5eece432c12bc532531c701f266
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:39:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = '组织信息被用来生成安全证书和其他软件的默认设置。';
+$lang['organization_app_name'] = '组织';
+$lang['organization_city'] = '市';
+$lang['organization_city_invalid'] = '市无效。';
+$lang['organization_country'] = '国家';
+$lang['organization_country_invalid'] = '国家是无效的。';
+$lang['organization_organization_invalid'] = '组织是无效的。';
+$lang['organization_organization'] = '组织';
+$lang['organization_postal_code_invalid'] = '邮政编码无效。';
+$lang['organization_postal_code'] = '邮政/邮编';
+$lang['organization_region_invalid'] = '国家，省或地区是无效的。';
+$lang['organization_region'] = '国家，省或地区';
+$lang['organization_street_address_invalid'] = '街道地址无效。';
+$lang['organization_street_address'] = '街道地址';
+$lang['organization_unit_invalid'] = '单位是无效的。';
+$lang['organization_unit'] = '单元';
