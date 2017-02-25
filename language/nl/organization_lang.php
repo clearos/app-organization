6b4b40da53a216829ef735e1f9840821ea829b55
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:44:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Informatie over uw organisatie wordt gebruikt om beveiligingscertificaten en andere software defaults te genereren.';
+$lang['organization_app_name'] = 'Organisatie';
+$lang['organization_city'] = 'stad';
+$lang['organization_city_invalid'] = 'City is ongeldig.';
+$lang['organization_country'] = 'land';
+$lang['organization_country_invalid'] = 'Country is ongeldig.';
+$lang['organization_organization_invalid'] = 'Organisatie is ongeldig.';
+$lang['organization_organization'] = 'Organisatie';
+$lang['organization_postal_code_invalid'] = 'Postcode is ongeldig.';
+$lang['organization_postal_code'] = 'Postcode';
+$lang['organization_region_invalid'] = 'Staat, provincie of regio is ongeldig.';
+$lang['organization_region'] = 'Staat, Provincie of Regio';
+$lang['organization_street_address_invalid'] = 'Adres is ongeldig.';
+$lang['organization_street_address'] = 'Adres';
+$lang['organization_unit_invalid'] = 'Unit is ongeldig.';
+$lang['organization_unit'] = 'Eenheid';
