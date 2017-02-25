862153e23e617f0a2e3ef373cff9db5b6c124e82
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:46:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Tlhahisoleseding e mabapi le mokhatlo oa lōna e sebelisoa ho hlahisa disetifikeiti sireletsehile &#39;me defaults tse ling Software.';
+$lang['organization_app_name'] = 'mokhatlo o hlophisitsoeng oa';
+$lang['organization_city'] = 'City';
+$lang['organization_city_invalid'] = 'City ha o sebetse.';
+$lang['organization_country'] = 'naha';
+$lang['organization_country_invalid'] = 'Naha ha o sebetse.';
+$lang['organization_organization_invalid'] = 'Mokhatlo o hlophisitsoeng oa ha o sebetse.';
+$lang['organization_organization'] = 'mokhatlo o hlophisitsoeng oa';
+$lang['organization_postal_code_invalid'] = 'Khoutu ya poso ha o sebetse.';
+$lang['organization_postal_code'] = 'Postal / Zip Code';
+$lang['organization_region_invalid'] = 'State, Province kapa Region ha o sebetse.';
+$lang['organization_region'] = 'State, Province kapa Region';
+$lang['organization_street_address_invalid'] = 'Street aterese ha o sebetse.';
+$lang['organization_street_address'] = 'Aterese ea seterata';
+$lang['organization_unit_invalid'] = 'Unit ha o sebetse.';
+$lang['organization_unit'] = 'Unit';
