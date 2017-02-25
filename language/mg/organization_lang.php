9c5958525c7f20270a065e968b435def29d94d16
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:43:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Information momba ny fandaminana no ampiasaina mba hiteraka fiarovana sy ny rindrambaiko taratasy fanamarinana defaults.';
+$lang['organization_app_name'] = 'fikambanana';
+$lang['organization_city'] = 'City';
+$lang['organization_city_invalid'] = 'City dia tsy manan-kery.';
+$lang['organization_country'] = 'Firenena';
+$lang['organization_country_invalid'] = 'Firenena dia tsy manan-kery.';
+$lang['organization_organization_invalid'] = 'Fikambanana dia tsy manan-kery.';
+$lang['organization_organization'] = 'fikambanana';
+$lang['organization_postal_code_invalid'] = 'Ny haavon&#39;ny dia tsy manan-kery.';
+$lang['organization_postal_code'] = 'Postal / Zip Code';
+$lang['organization_region_invalid'] = 'State, Province na dia tsy manan-kery amin&#39;ny faritr&#39;i.';
+$lang['organization_region'] = 'State, Province na Region';
+$lang['organization_street_address_invalid'] = 'Street adiresy dia tsy manan-kery.';
+$lang['organization_street_address'] = 'Adiresin&#39;ny làlana';
+$lang['organization_unit_invalid'] = 'Unit dia tsy manan-kery.';
+$lang['organization_unit'] = 'Unit';
