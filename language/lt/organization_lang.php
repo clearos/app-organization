2a5fb1b0bcda49d810c76a8419e9b2a42ce0a193
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:43:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Informacija apie jūsų organizaciją naudojamas generuoti saugumo sertifikatus ir kita programinė įranga nutylėjimą.';
+$lang['organization_app_name'] = 'organizacija';
+$lang['organization_city'] = 'miestas';
+$lang['organization_city_invalid'] = 'Miestas yra neteisinga.';
+$lang['organization_country'] = 'Šalis';
+$lang['organization_country_invalid'] = 'Šalis yra neteisinga.';
+$lang['organization_organization_invalid'] = 'Organizacija yra neteisinga.';
+$lang['organization_organization'] = 'organizacija';
+$lang['organization_postal_code_invalid'] = 'Pašto kodas yra neteisingas.';
+$lang['organization_postal_code'] = 'Pašto / Pašto kodas';
+$lang['organization_region_invalid'] = 'Valstybė, provincija arba regionas yra negaliojantis.';
+$lang['organization_region'] = 'Valstybė, provincija arba regionas';
+$lang['organization_street_address_invalid'] = 'Gatvė adresas yra neteisingas.';
+$lang['organization_street_address'] = 'Gatvės adresas';
+$lang['organization_unit_invalid'] = 'Vienetas yra neteisinga.';
+$lang['organization_unit'] = 'vienetas';
