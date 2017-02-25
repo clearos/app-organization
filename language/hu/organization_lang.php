6939617c3e59728e78c74c4a273644ae1b398fd3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:41:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Információ a szervezet előállításánál használt biztonsági tanúsítványok és egyéb szoftverek alapértékekre.';
+$lang['organization_app_name'] = 'Szervezet';
+$lang['organization_city'] = 'Város';
+$lang['organization_city_invalid'] = 'Város érvénytelen.';
+$lang['organization_country'] = 'Ország';
+$lang['organization_country_invalid'] = 'Ország érvénytelen.';
+$lang['organization_organization_invalid'] = 'Szervezet érvénytelen.';
+$lang['organization_organization'] = 'Szervezet';
+$lang['organization_postal_code_invalid'] = 'Irányítószám érvénytelen.';
+$lang['organization_postal_code'] = 'Postai / irányítószám';
+$lang['organization_region_invalid'] = 'Állam, megye vagy régió érvénytelen.';
+$lang['organization_region'] = 'Állam, Tartomány vagy Régió';
+$lang['organization_street_address_invalid'] = 'Street cím érvénytelen.';
+$lang['organization_street_address'] = 'Cím';
+$lang['organization_unit_invalid'] = 'Egység érvénytelen.';
+$lang['organization_unit'] = 'Egység';
