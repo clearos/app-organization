45d87abcb731b69d7c3efbaf2128e35d271274ff
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:38:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'আপনার প্রতিষ্ঠান সম্পর্কে তথ্য নিরাপত্তা সার্টিফিকেট ও অন্যান্য সফ্টওয়্যার ডিফল্ট জেনারেট করতে ব্যবহৃত হয়.';
+$lang['organization_app_name'] = 'সংগঠন';
+$lang['organization_city'] = 'শহর';
+$lang['organization_city_invalid'] = 'সিটি অবৈধ.';
+$lang['organization_country'] = 'দেশ';
+$lang['organization_country_invalid'] = 'দেশ অবৈধ.';
+$lang['organization_organization_invalid'] = 'অর্গানাইজেশন অবৈধ.';
+$lang['organization_organization'] = 'সংগঠন';
+$lang['organization_postal_code_invalid'] = 'পোস্টাল কোড অবৈধ.';
+$lang['organization_postal_code'] = 'পোস্টকোড / জিপকোড';
+$lang['organization_region_invalid'] = 'রাজ্য, প্রদেশ বা অঞ্চলের অবৈধ.';
+$lang['organization_region'] = 'রাজ্য, প্রদেশ বা অঞ্চল';
+$lang['organization_street_address_invalid'] = 'রাস্তার ঠিকানা সঠিক নয়.';
+$lang['organization_street_address'] = 'রাস্তার ঠিকানা';
+$lang['organization_unit_invalid'] = 'ইউনিট অবৈধ.';
+$lang['organization_unit'] = 'একক';
