4469c4c1f8e3ee03d9759165b9b26581d5db3c53
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:42:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'ინფორმაცია თქვენი ორგანიზაციის გამოიყენება გენერირება უსაფრთხოების სერთიფიკატები და სხვა პროგრამული უზრუნველყოფა დეფოლტის.';
+$lang['organization_app_name'] = 'ორგანიზაცია';
+$lang['organization_city'] = 'ქალაქი';
+$lang['organization_city_invalid'] = 'ქალაქი არასწორია.';
+$lang['organization_country'] = 'ქვეყანა';
+$lang['organization_country_invalid'] = 'ქვეყანა არასწორია.';
+$lang['organization_organization_invalid'] = 'ორგანიზაცია არასწორია.';
+$lang['organization_organization'] = 'ორგანიზაცია';
+$lang['organization_postal_code_invalid'] = 'საფოსტო კოდი არასწორია.';
+$lang['organization_postal_code'] = 'საფოსტო / Zip Code';
+$lang['organization_region_invalid'] = 'სახელმწიფო, მხარის ან რეგიონის არასწორია.';
+$lang['organization_region'] = 'სახელმწიფო, მხარის ან რეგიონის';
+$lang['organization_street_address_invalid'] = 'ქუჩის მისამართი არასწორია.';
+$lang['organization_street_address'] = 'ქუჩის მისამართი';
+$lang['organization_unit_invalid'] = 'განყოფილება არასწორია.';
+$lang['organization_unit'] = 'განყოფილება';
