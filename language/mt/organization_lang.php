5175875751a25fbcee7bae486729dcb27630f6f3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:44:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Informazzjoni dwar l-organizzazzjoni tiegħek hija użata biex tiġġenera ċertifikati ta &#39;sigurtà u nuqqasijiet oħra ta&#39; software.';
+$lang['organization_app_name'] = 'organizzazzjoni';
+$lang['organization_city'] = 'belt';
+$lang['organization_city_invalid'] = 'City hija invalida.';
+$lang['organization_country'] = 'pajjiż';
+$lang['organization_country_invalid'] = 'Pajjiż huwa invalidu.';
+$lang['organization_organization_invalid'] = 'Organizzazzjoni huwa invalidu.';
+$lang['organization_organization'] = 'organizzazzjoni';
+$lang['organization_postal_code_invalid'] = 'Kodiċi postali huwa invalidu.';
+$lang['organization_postal_code'] = 'Postali / Zip Kodiċi';
+$lang['organization_region_invalid'] = 'Istat, Provinċja jew Reġjun huwa invalidu.';
+$lang['organization_region'] = 'Istat, Provinċja jew Reġjun';
+$lang['organization_street_address_invalid'] = 'Indirizz tat-triq huwa invalidu.';
+$lang['organization_street_address'] = 'Indirizz ta &#39;t-triq';
+$lang['organization_unit_invalid'] = 'Unità hija invalida.';
+$lang['organization_unit'] = 'unità';
