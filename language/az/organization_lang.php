adcf4605efbe230854d91744e47462e766c22cc9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:38:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Sizin təşkilat haqqında məlumat təhlükəsizlik sertifikatları və digər proqram yetirilməməsi yaratmaq üçün istifadə olunur.';
+$lang['organization_app_name'] = 'Təşkilat';
+$lang['organization_city'] = 'şəhər';
+$lang['organization_city_invalid'] = 'Şəhər yanlışdır.';
+$lang['organization_country'] = 'ölkə';
+$lang['organization_country_invalid'] = 'Ölkə yanlışdır.';
+$lang['organization_organization_invalid'] = 'Organization yanlışdır.';
+$lang['organization_organization'] = 'Təşkilat';
+$lang['organization_postal_code_invalid'] = 'Saxlayın Kodu yanlışdır.';
+$lang['organization_postal_code'] = 'Poçt / Zip Code';
+$lang['organization_region_invalid'] = 'Dövlət, Province və ya region yanlışdır.';
+$lang['organization_region'] = 'Dövlət, Province və ya region';
+$lang['organization_street_address_invalid'] = 'Ünvanı, yanlışdır.';
+$lang['organization_street_address'] = 'Küçə ünvanı';
+$lang['organization_unit_invalid'] = 'Unit yanlışdır.';
+$lang['organization_unit'] = 'vahid';
