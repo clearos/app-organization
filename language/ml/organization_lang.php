97212d4a072cb5fcc1cdcb3e66ac3e38a2b2456a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:44:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'നിങ്ങളുടെ സംഘടനയെക്കുറിച്ച് ഇൻഫർമേഷൻ സെക്യൂരിറ്റി സർട്ടിഫിക്കറ്റുകൾ മറ്റ് സോഫ്റ്റ്വെയർ സ്ഥിരസ്ഥിതികൾ ജനറേറ്റ് ഉപയോഗിക്കുന്നു.';
+$lang['organization_app_name'] = 'സംഘടന';
+$lang['organization_city'] = 'നഗരം';
+$lang['organization_city_invalid'] = 'സിറ്റി അസാധുവാണ്.';
+$lang['organization_country'] = 'രാജ്യം';
+$lang['organization_country_invalid'] = 'രാജ്യം അസാധുവാണ്.';
+$lang['organization_organization_invalid'] = 'ഓർഗനൈസേഷൻ അസാധുവാണ്.';
+$lang['organization_organization'] = 'സംഘടന';
+$lang['organization_postal_code_invalid'] = 'തപാൽ കോഡ് അസാധുവാണ്.';
+$lang['organization_postal_code'] = 'തപാൽ / പിൻ കോഡ്';
+$lang['organization_region_invalid'] = 'സംസ്ഥാന, പ്രവിശ്യ അല്ലെങ്കിൽ പ്രദേശം അസാധുവാണ്.';
+$lang['organization_region'] = 'സംസ്ഥാന, പ്രവിശ്യ അല്ലെങ്കിൽ പ്രദേശം';
+$lang['organization_street_address_invalid'] = 'സ്ട്രീറ്റ് വിലാസം അസാധുവാണ്.';
+$lang['organization_street_address'] = 'സ്ട്രീറ്റ് വിലാസം';
+$lang['organization_unit_invalid'] = 'ഭാഗം അസാധുവാണ്.';
+$lang['organization_unit'] = 'ഘടകം';
