c6702208f08e1a78b722cd89ddf0d054529c6da9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:38:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Informacije o vašoj organizaciji se koristi za generiranje sigurnosne certifikate i drugi softver vrednosti.';
+$lang['organization_app_name'] = 'organizacija';
+$lang['organization_city'] = 'grad';
+$lang['organization_city_invalid'] = 'Grad je nevažeći.';
+$lang['organization_country'] = 'zemlja';
+$lang['organization_country_invalid'] = 'Zemlja je nevažeći.';
+$lang['organization_organization_invalid'] = 'Organizacija je nevažeći.';
+$lang['organization_organization'] = 'organizacija';
+$lang['organization_postal_code_invalid'] = 'Poštanski broj je nevažeći.';
+$lang['organization_postal_code'] = 'Poštanski / Zip Code';
+$lang['organization_region_invalid'] = 'Država, pokrajina ili regije je nevažeća.';
+$lang['organization_region'] = 'Država, pokrajina ili region';
+$lang['organization_street_address_invalid'] = 'Ulica je nevažeća.';
+$lang['organization_street_address'] = 'Ulica';
+$lang['organization_unit_invalid'] = 'Jedinica je nevažeći.';
+$lang['organization_unit'] = 'jedinica';
