376dc85ebbe08fc075f714ca9fc5fdc0c922fb1b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:44:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'танай байгууллагын тухай мэдээлэл, аюулгүй байдлын гэрчилгээ болон бусад програм хангамжийн анхдагчуудыг үүсгэхийн тулд ашиглаж байна.';
+$lang['organization_app_name'] = 'Байгууллага';
+$lang['organization_city'] = 'City';
+$lang['organization_city_invalid'] = 'Хот хүчин төгөлдөр бус байна.';
+$lang['organization_country'] = 'Улс';
+$lang['organization_country_invalid'] = 'Улс хүчин төгөлдөр бус байна.';
+$lang['organization_organization_invalid'] = 'Байгууллага хүчин төгөлдөр бус байна.';
+$lang['organization_organization'] = 'Байгууллага';
+$lang['organization_postal_code_invalid'] = 'Шуудангийн код хүчингүй байна.';
+$lang['organization_postal_code'] = 'Шуудангийн / ZIP код';
+$lang['organization_region_invalid'] = 'Төрийн, аймаг, бүс нутгийн хүчин төгөлдөр бус байна.';
+$lang['organization_region'] = 'Төрийн, аймаг, бүс нутаг';
+$lang['organization_street_address_invalid'] = 'Гудамжны хаяг хүчин төгөлдөр бус байна.';
+$lang['organization_street_address'] = 'Гудамжны хаяг';
+$lang['organization_unit_invalid'] = 'Нэгж хүчин төгөлдөр бус байна.';
+$lang['organization_unit'] = 'Нэгж';
