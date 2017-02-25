b587a423ad26f93b842bf566120eac7d776e559f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:44:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'आपल्या संस्थेबद्दल माहिती सुरक्षा प्रमाणपत्रे आणि इतर सॉफ्टवेअर पूर्वनिर्धारित निर्माण केला जातो.';
+$lang['organization_app_name'] = 'संघटना';
+$lang['organization_city'] = 'सिटी';
+$lang['organization_city_invalid'] = 'शहर अवैध आहे.';
+$lang['organization_country'] = 'देश';
+$lang['organization_country_invalid'] = 'देश अवैध आहे.';
+$lang['organization_organization_invalid'] = 'संघटना अवैध आहे.';
+$lang['organization_organization'] = 'संघटना';
+$lang['organization_postal_code_invalid'] = 'पोस्टल कोड अवैध आहे.';
+$lang['organization_postal_code'] = 'पोस्टल / पिन कोड';
+$lang['organization_region_invalid'] = 'राज्य, प्रांत किंवा प्रदेश अवैध आहे.';
+$lang['organization_region'] = 'राज्य, प्रांत किंवा प्रदेश';
+$lang['organization_street_address_invalid'] = 'मार्ग पत्ता अवैध आहे.';
+$lang['organization_street_address'] = 'मार्ग पत्ता';
+$lang['organization_unit_invalid'] = 'युनिट अवैध आहे.';
+$lang['organization_unit'] = 'युनिट';
