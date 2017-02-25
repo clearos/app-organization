dfa1061d5ec474e126cc9d359d71fc22a24e4481
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:41:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Informasi tentang organisasi Anda digunakan untuk menghasilkan sertifikat keamanan dan default perangkat lunak lain.';
+$lang['organization_app_name'] = 'Organisasi';
+$lang['organization_city'] = 'Kota';
+$lang['organization_city_invalid'] = 'Kota tidak valid.';
+$lang['organization_country'] = 'Negara';
+$lang['organization_country_invalid'] = 'Negara tidak valid.';
+$lang['organization_organization_invalid'] = 'Organisasi tidak valid.';
+$lang['organization_organization'] = 'Organisasi';
+$lang['organization_postal_code_invalid'] = 'kode pos tidak valid.';
+$lang['organization_postal_code'] = 'Pos / Zip Kode';
+$lang['organization_region_invalid'] = 'Negara, Provinsi atau Daerah tidak valid.';
+$lang['organization_region'] = 'Negara, Provinsi atau Daerah';
+$lang['organization_street_address_invalid'] = 'alamat jalan tidak valid.';
+$lang['organization_street_address'] = 'Alamat jalan';
+$lang['organization_unit_invalid'] = 'Unit tidak valid.';
+$lang['organization_unit'] = 'Satuan';
