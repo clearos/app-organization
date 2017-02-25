144ff90b2229bdd04c51d09c67955bf6d05bcb1b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:43:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'ຂໍ້ມູນກ່ຽວກັບອົງການຈັດຕັ້ງຂອງທ່ານໄດ້ຖືກນໍາໃຊ້ເພື່ອສ້າງໃບຢັ້ງຢືນຄວາມປອດໄພແລະຄ່າເລີ່ມຕົ້ນຊອບແວອື່ນໆ.';
+$lang['organization_app_name'] = 'ອົງການຈັດຕັ້ງ';
+$lang['organization_city'] = 'ເມືອງ';
+$lang['organization_city_invalid'] = 'ນະຄອນແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['organization_country'] = 'ປະເທດ';
+$lang['organization_country_invalid'] = 'ປະເທດບໍ່ຖືກຕ້ອງ.';
+$lang['organization_organization_invalid'] = 'ອົງການຈັດຕັ້ງແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['organization_organization'] = 'ອົງການຈັດຕັ້ງ';
+$lang['organization_postal_code_invalid'] = 'ລະຫັດໄປສະນີແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['organization_postal_code'] = 'ໄປສະນີ / ລະຫັດໄປສະນີ';
+$lang['organization_region_invalid'] = 'ລັດ, ຈັງຫວັດຫລື Region ແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['organization_region'] = 'ລັດ, ຈັງຫວັດຫລື Region';
+$lang['organization_street_address_invalid'] = 'ທີ່ຢູ່ຕາມຖະຫນົນແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['organization_street_address'] = 'ທີ່​ຢູ່​ຖະ​ຫນົນ';
+$lang['organization_unit_invalid'] = 'ຫນ່ວຍບໍລິການບໍ່ຖືກຕ້ອງ.';
+$lang['organization_unit'] = 'ຫນ່ວຍບໍລິການ';
