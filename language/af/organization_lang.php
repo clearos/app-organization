bec63fd2562ab8527dc54112c8f61217c837e34d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:37:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Inligting oor jou organisasie gebruik word om sekuriteit sertifikate en ander sagteware standaard te genereer.';
+$lang['organization_app_name'] = 'organisasie';
+$lang['organization_city'] = 'Stad';
+$lang['organization_city_invalid'] = 'Plaas is ongeldig.';
+$lang['organization_country'] = 'land';
+$lang['organization_country_invalid'] = 'Land is ongeldig.';
+$lang['organization_organization_invalid'] = 'Organisasie is ongeldig.';
+$lang['organization_organization'] = 'organisasie';
+$lang['organization_postal_code_invalid'] = 'Poskode is ongeldig.';
+$lang['organization_postal_code'] = 'Postal / Poskode';
+$lang['organization_region_invalid'] = 'State, provinsie of streek is ongeldig.';
+$lang['organization_region'] = 'State, provinsie of streek';
+$lang['organization_street_address_invalid'] = 'Straat adres is ongeldig.';
+$lang['organization_street_address'] = 'Straatadres';
+$lang['organization_unit_invalid'] = 'Eenheid is ongeldig.';
+$lang['organization_unit'] = 'eenheid';
