5b54f4c76f72dabbfb1960515523f29149abece5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:43:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'whakamahia ngā mōhiohio e pā ana ki to koutou whakahaere ki te whakaputa tiwhikete haumaru me ētahi atu taunoa pūmanawa.';
+$lang['organization_app_name'] = 'Organization';
+$lang['organization_city'] = 'City';
+$lang['organization_city_invalid'] = 'He muhu pa.';
+$lang['organization_country'] = 'whenua';
+$lang['organization_country_invalid'] = 'He muhu Country.';
+$lang['organization_organization_invalid'] = 'He muhu Organization.';
+$lang['organization_organization'] = 'Organization';
+$lang['organization_postal_code_invalid'] = 'He muhu te waehere poutāpeta.';
+$lang['organization_postal_code'] = '-Pōhi / Poutāpeta Waehere';
+$lang['organization_region_invalid'] = 'State, he muhu Porowini Region ranei.';
+$lang['organization_region'] = 'State, Porowini Region ranei';
+$lang['organization_street_address_invalid'] = 'He muhu wāhitau Street.';
+$lang['organization_street_address'] = 'Wāhitau Street';
+$lang['organization_unit_invalid'] = 'He muhu Unit.';
+$lang['organization_unit'] = 'Unit';
