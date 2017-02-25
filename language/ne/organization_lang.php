0104a77aead68dbeec79944c5b1bfd68687c20fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:44:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'आफ्नो संगठन बारेमा जानकारी सुरक्षा प्रमाणपत्र र अन्य पूर्वनिर्धारित सफ्टवेयर उत्पन्न गर्न प्रयोग गरिएको छ।';
+$lang['organization_app_name'] = 'संगठन';
+$lang['organization_city'] = 'शहर';
+$lang['organization_city_invalid'] = 'सिटी अमान्य छ।';
+$lang['organization_country'] = 'देश';
+$lang['organization_country_invalid'] = 'देश अमान्य छ।';
+$lang['organization_organization_invalid'] = 'संगठन अमान्य छ।';
+$lang['organization_organization'] = 'संगठन';
+$lang['organization_postal_code_invalid'] = 'हुलाक कोड अवैध छ।';
+$lang['organization_postal_code'] = 'हुलाक / जिप कोड';
+$lang['organization_region_invalid'] = 'राज्य, प्रान्त वा क्षेत्र अमान्य छ।';
+$lang['organization_region'] = 'राज्य, प्रान्त वा क्षेत्र';
+$lang['organization_street_address_invalid'] = 'सडक ठेगाना अमान्य छ।';
+$lang['organization_street_address'] = 'सडक ठेगाना';
+$lang['organization_unit_invalid'] = 'एकाइ अमान्य छ।';
+$lang['organization_unit'] = 'एकाइ';
