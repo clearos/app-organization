03048131d87faafbe5f10f249346a9359e8dd848
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:47:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'ข้อมูลเกี่ยวกับองค์กรของคุณจะถูกใช้ในการสร้างใบรับรองความปลอดภัยและค่าเริ่มต้นซอฟต์แวร์อื่น ๆ';
+$lang['organization_app_name'] = 'องค์กร';
+$lang['organization_city'] = 'เมือง';
+$lang['organization_city_invalid'] = 'เมืองที่ไม่ถูกต้อง';
+$lang['organization_country'] = 'ประเทศ';
+$lang['organization_country_invalid'] = 'ประเทศไม่ถูกต้อง';
+$lang['organization_organization_invalid'] = 'องค์การไม่ถูกต้อง';
+$lang['organization_organization'] = 'องค์กร';
+$lang['organization_postal_code_invalid'] = 'รหัสไปรษณีย์ไม่ถูกต้อง';
+$lang['organization_postal_code'] = 'ไปรษณีย์ / รหัสไปรษณีย์';
+$lang['organization_region_invalid'] = 'รัฐจังหวัดหรือภูมิภาคไม่ถูกต้อง';
+$lang['organization_region'] = 'รัฐจังหวัดหรือภูมิภาค';
+$lang['organization_street_address_invalid'] = 'ที่อยู่ไม่ถูกต้อง';
+$lang['organization_street_address'] = 'ที่อยู่ถนน';
+$lang['organization_unit_invalid'] = 'หน่วยไม่ถูกต้อง';
+$lang['organization_unit'] = 'หน่วย';
