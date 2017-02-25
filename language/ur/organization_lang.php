36fd8dfb1393f1c8c67473d498dacc773831183b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:48:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'آپ کی تنظیم کے بارے میں معلومات کی سیکورٹی اسناد اور دوسرے سافٹ ویئر ڈیفالٹس پیدا کرنے کے لئے استعمال کیا جاتا ہے.';
+$lang['organization_app_name'] = 'ادارہ';
+$lang['organization_city'] = 'شہر';
+$lang['organization_city_invalid'] = 'سٹی غلط ہے.';
+$lang['organization_country'] = 'ملک';
+$lang['organization_country_invalid'] = 'ملک غلط ہے.';
+$lang['organization_organization_invalid'] = 'تنظیم غلط ہے.';
+$lang['organization_organization'] = 'ادارہ';
+$lang['organization_postal_code_invalid'] = 'پوسٹل کوڈ درست نہیں ہے.';
+$lang['organization_postal_code'] = 'ڈاک / زپ کوڈ';
+$lang['organization_region_invalid'] = 'ریاست، صوبہ یا علاقہ غلط ہے.';
+$lang['organization_region'] = 'ریاست، صوبہ یا علاقہ';
+$lang['organization_street_address_invalid'] = 'سٹریٹ پتہ غلط ہے.';
+$lang['organization_street_address'] = 'گلی کا پتہ';
+$lang['organization_unit_invalid'] = 'یونٹ غلط ہے.';
+$lang['organization_unit'] = 'اکائی';
