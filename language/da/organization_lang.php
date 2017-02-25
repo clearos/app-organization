e0117595456d5492e16ed5aa1bffc69e6d5c28ef
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:39:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Oplysninger om din organisation bruges til at generere sikkerhedscertifikater og andre software standardindstillinger.';
+$lang['organization_app_name'] = 'Organisation';
+$lang['organization_city'] = 'by';
+$lang['organization_city_invalid'] = 'City er ugyldig.';
+$lang['organization_country'] = 'Land';
+$lang['organization_country_invalid'] = 'Country er ugyldig.';
+$lang['organization_organization_invalid'] = 'Organisationen er ugyldig.';
+$lang['organization_organization'] = 'Organisation';
+$lang['organization_postal_code_invalid'] = 'Postnr er ugyldig.';
+$lang['organization_postal_code'] = 'Postal / Zip Code';
+$lang['organization_region_invalid'] = 'Stat, provins eller region er ugyldig.';
+$lang['organization_region'] = 'Stat, provins eller region';
+$lang['organization_street_address_invalid'] = 'Street adresse er ugyldig.';
+$lang['organization_street_address'] = 'Vejnavn';
+$lang['organization_unit_invalid'] = 'Enhed er ugyldig.';
+$lang['organization_unit'] = 'Enhed';
