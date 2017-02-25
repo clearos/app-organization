63e7cb137bc528dd16d35a9f9315637ab3948543
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:47:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Information om din organisation används för att generera säkerhetscertifikat och andra programinställningar.';
+$lang['organization_app_name'] = 'Organisation';
+$lang['organization_city'] = 'Stad';
+$lang['organization_city_invalid'] = 'City är ogiltig.';
+$lang['organization_country'] = 'Land';
+$lang['organization_country_invalid'] = 'Land är ogiltig.';
+$lang['organization_organization_invalid'] = 'Organisationen är ogiltig.';
+$lang['organization_organization'] = 'Organisation';
+$lang['organization_postal_code_invalid'] = 'Postnummer är ogiltig.';
+$lang['organization_postal_code'] = 'Postnummer';
+$lang['organization_region_invalid'] = 'State, är Province eller region ogiltig.';
+$lang['organization_region'] = 'Stat, Provins eller Region';
+$lang['organization_street_address_invalid'] = 'Gatuadress är ogiltig.';
+$lang['organization_street_address'] = 'Gatuadress';
+$lang['organization_unit_invalid'] = 'Enheten är ogiltig.';
+$lang['organization_unit'] = 'Enhet';
