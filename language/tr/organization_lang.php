402d4e68a2c55aa5580b8b3ef6e6f375af975ed0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:48:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'kuruluşunuz hakkında bilgi güvenlik sertifikaları ve diğer yazılım varsayılan oluşturmak için kullanılır.';
+$lang['organization_app_name'] = 'organizasyon';
+$lang['organization_city'] = 'Şehir';
+$lang['organization_city_invalid'] = 'Şehir geçersiz.';
+$lang['organization_country'] = 'ülke';
+$lang['organization_country_invalid'] = 'Ülke geçersiz.';
+$lang['organization_organization_invalid'] = 'Organizasyon geçersiz.';
+$lang['organization_organization'] = 'organizasyon';
+$lang['organization_postal_code_invalid'] = 'Posta kodu geçersiz.';
+$lang['organization_postal_code'] = 'Posta / Posta Kodu';
+$lang['organization_region_invalid'] = 'Devlet, İl ya da Bölge geçersiz.';
+$lang['organization_region'] = 'Devlet, İl ya da Bölge';
+$lang['organization_street_address_invalid'] = 'Sokak adresi geçersiz.';
+$lang['organization_street_address'] = 'Açık adres';
+$lang['organization_unit_invalid'] = 'Birim geçersiz.';
+$lang['organization_unit'] = 'birim';
