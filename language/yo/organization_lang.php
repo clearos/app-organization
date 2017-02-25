9bd6f8cabac263a91c40aa218855d474e8db3bff
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:49:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Alaye nipa rẹ ètò ti wa ni lo lati se ina aabo-ẹri ati awọn miiran software aseku.';
+$lang['organization_app_name'] = 'Organization';
+$lang['organization_city'] = 'City';
+$lang['organization_city_invalid'] = 'City ni invalid.';
+$lang['organization_country'] = 'orilẹ-ede';
+$lang['organization_country_invalid'] = 'Orilẹ-ede ti wa ni invalid.';
+$lang['organization_organization_invalid'] = 'Organization ni invalid.';
+$lang['organization_organization'] = 'Organization';
+$lang['organization_postal_code_invalid'] = 'Koodu ifiweranse ni invalid.';
+$lang['organization_postal_code'] = 'Ifiweranse / Zip koodu';
+$lang['organization_region_invalid'] = 'Ipinle, ekun tabi Region ni invalid.';
+$lang['organization_region'] = 'Ipinle, ekun tabi Region';
+$lang['organization_street_address_invalid'] = 'Ita adirẹsi ni invalid.';
+$lang['organization_street_address'] = 'Adirẹsi opopona';
+$lang['organization_unit_invalid'] = 'Kuro ni invalid.';
+$lang['organization_unit'] = 'Unit';
