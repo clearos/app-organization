e07b07554b6b9116e2e31a3512222594994ec621
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:44:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Maklumat mengenai organisasi anda digunakan untuk menjana sijil keselamatan dan keingkaran perisian lain.';
+$lang['organization_app_name'] = 'pertubuhan';
+$lang['organization_city'] = 'City';
+$lang['organization_city_invalid'] = 'City tidak sah.';
+$lang['organization_country'] = 'negara';
+$lang['organization_country_invalid'] = 'Negara tidak sah.';
+$lang['organization_organization_invalid'] = 'Organisasi adalah tidak sah.';
+$lang['organization_organization'] = 'pertubuhan';
+$lang['organization_postal_code_invalid'] = 'kod pos tidak sah.';
+$lang['organization_postal_code'] = 'Poskod / Zip';
+$lang['organization_region_invalid'] = 'Negeri, Negeri atau Wilayah adalah tidak sah.';
+$lang['organization_region'] = 'Negeri, Negeri atau Wilayah';
+$lang['organization_street_address_invalid'] = 'alamat jalan tidak sah.';
+$lang['organization_street_address'] = 'Alamat jalan';
+$lang['organization_unit_invalid'] = 'Unit tidak sah.';
+$lang['organization_unit'] = 'Unit';
