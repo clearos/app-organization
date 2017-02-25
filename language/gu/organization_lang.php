e27b6f652aed93c222b9bcc0f7c220d29a3895c7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:41:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'તમારી સંસ્થા વિશે માહિતી સુરક્ષા પ્રમાણપત્રો અને અન્ય સોફ્ટવેર મૂળભૂતો પેદા કરવા માટે વપરાય છે.';
+$lang['organization_app_name'] = 'સંસ્થા';
+$lang['organization_city'] = 'સિટી';
+$lang['organization_city_invalid'] = 'સિટી અમાન્ય છે.';
+$lang['organization_country'] = 'દેશ';
+$lang['organization_country_invalid'] = 'દેશ અમાન્ય છે.';
+$lang['organization_organization_invalid'] = 'સંસ્થા અમાન્ય છે.';
+$lang['organization_organization'] = 'સંસ્થા';
+$lang['organization_postal_code_invalid'] = 'પોસ્ટલ કોડ અમાન્ય છે.';
+$lang['organization_postal_code'] = 'ટપાલ / પીન કોડ';
+$lang['organization_region_invalid'] = 'રાજ્ય, પ્રાંત અથવા વિસ્તાર અમાન્ય છે.';
+$lang['organization_region'] = 'રાજ્ય, પ્રાંત અથવા વિસ્તાર';
+$lang['organization_street_address_invalid'] = 'સ્ટ્રીટ સરનામું અમાન્ય છે.';
+$lang['organization_street_address'] = 'શેરીનુ સરનામું';
+$lang['organization_unit_invalid'] = 'એકમ અમાન્ય છે.';
+$lang['organization_unit'] = 'એકમ';
