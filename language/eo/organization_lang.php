927d76f2649d9e66b4436de17f60ec2426f816cc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:40:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Informoj pri via organizo estas uzata por generi sekureco atestilojn kaj aliaj softvaro defaŭltoj.';
+$lang['organization_app_name'] = 'Organizo';
+$lang['organization_city'] = 'urbo';
+$lang['organization_city_invalid'] = 'Urbo estas nevalida.';
+$lang['organization_country'] = 'lando';
+$lang['organization_country_invalid'] = 'Lando estas nevalidaj.';
+$lang['organization_organization_invalid'] = 'Organizo estas nevalida.';
+$lang['organization_organization'] = 'Organizo';
+$lang['organization_postal_code_invalid'] = 'Poŝtkodo estas nevalida.';
+$lang['organization_postal_code'] = 'Poŝta / Poŝtkodo';
+$lang['organization_region_invalid'] = 'Stato, provinco aŭ Regiono estas nevalida.';
+$lang['organization_region'] = 'Stato, provinco aŭ Regiono';
+$lang['organization_street_address_invalid'] = 'Strato adreso estas nevalida.';
+$lang['organization_street_address'] = 'Strato Adreso';
+$lang['organization_unit_invalid'] = 'Unueco estas nevalida.';
+$lang['organization_unit'] = 'unuobla';
