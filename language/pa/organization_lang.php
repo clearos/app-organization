481d30020ad67bf49902f0bd32f95734ff9705e8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:45:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'ਆਪਣੇ ਸੰਗਠਨ ਬਾਰੇ ਜਾਣਕਾਰੀ ਦੀ ਸੁਰੱਖਿਆ ਨੂੰ ਸਰਟੀਫਿਕੇਟ ਅਤੇ ਹੋਰ ਸਾਫਟਵੇਅਰ ਮੂਲ ਪੈਦਾ ਕਰਨ ਲਈ ਵਰਤਿਆ ਗਿਆ ਹੈ.';
+$lang['organization_app_name'] = 'ਸੰਗਠਨ';
+$lang['organization_city'] = 'ਸਿਟੀ';
+$lang['organization_city_invalid'] = 'ਸਿਟੀ ਗਲਤ ਹੈ.';
+$lang['organization_country'] = 'ਦੇਸ਼';
+$lang['organization_country_invalid'] = 'ਦੇਸ਼ ਗਲਤ ਹੈ.';
+$lang['organization_organization_invalid'] = 'ਸੰਗਠਨ ਗਲਤ ਹੈ.';
+$lang['organization_organization'] = 'ਸੰਗਠਨ';
+$lang['organization_postal_code_invalid'] = 'ਡਾਕ ਕੋਡ ਗਲਤ ਹੈ.';
+$lang['organization_postal_code'] = 'ਡਾਕ / ਜ਼ਿਪ ਕੋਡ';
+$lang['organization_region_invalid'] = 'ਰਾਜ, ਪ੍ਰੋਵਿੰਸ ਜ ਖੇਤਰ ਗਲਤ ਹੈ.';
+$lang['organization_region'] = 'ਰਾਜ, ਸੂਬੇ ਜ ਖੇਤਰ';
+$lang['organization_street_address_invalid'] = 'ਸਟਰੀਟ ਪਤੇ ਨੂੰ ਗਲਤ ਹੈ.';
+$lang['organization_street_address'] = 'ਸੜਕ ਦਾ ਪਤਾ';
+$lang['organization_unit_invalid'] = 'ਯੂਨਿਟ ਗਲਤ ਹੈ.';
+$lang['organization_unit'] = 'ਯੂਨਿਟ';
