4a1d1ec5aec0055da75c98a7d15d61195b559a7e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:42:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'អំពីអង្គការរបស់អ្នកគឺត្រូវបានប្រើដើម្បីបង្កើតវិញ្ញាបនបត្រសន្តិសុខនិងលំនាំដើមផ្នែកទន់ផ្សេងទៀត។';
+$lang['organization_app_name'] = 'អង្គការ';
+$lang['organization_city'] = 'ទីក្រុង';
+$lang['organization_city_invalid'] = 'ទីក្រុងមិនត្រឹមត្រូវ។';
+$lang['organization_country'] = 'ប្រទេស';
+$lang['organization_country_invalid'] = 'ប្រទេសមិនត្រឹមត្រូវ។';
+$lang['organization_organization_invalid'] = 'អង្គការមិនត្រឹមត្រូវ។';
+$lang['organization_organization'] = 'អង្គការ';
+$lang['organization_postal_code_invalid'] = 'លេខកូដប្រៃសណីយ៍មិនត្រឹមត្រូវ។';
+$lang['organization_postal_code'] = 'កូដប្រៃសណីយ៍ / តំបន់';
+$lang['organization_region_invalid'] = 'រដ្ឋ, ខេត្តឬតំបន់មិនត្រឹមត្រូវ។';
+$lang['organization_region'] = 'រដ្ឋ, ខេត្តឬតំបន់';
+$lang['organization_street_address_invalid'] = 'អាសយដ្ឋានផ្លូវមិនត្រឹមត្រូវ។';
+$lang['organization_street_address'] = 'អាស័យ​ដ្ឋាន​ផ្លូវ';
+$lang['organization_unit_invalid'] = 'អង្គភាពមិនត្រឹមត្រូវ។';
+$lang['organization_unit'] = 'អង្គភាព';
