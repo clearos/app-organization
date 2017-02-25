bddbd498bc245a159774190ae0505201e1733ea3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:46:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Informácie o vašej organizácii sa používa na výrobu bezpečnostných certifikátov a ďalší softvér predvolené nastavenia.';
+$lang['organization_app_name'] = 'organizácie';
+$lang['organization_city'] = 'veľkomesto';
+$lang['organization_city_invalid'] = 'Mesto je neplatný.';
+$lang['organization_country'] = 'Krajina';
+$lang['organization_country_invalid'] = 'Zem je neplatný.';
+$lang['organization_organization_invalid'] = 'Organizácia je neplatný.';
+$lang['organization_organization'] = 'organizácie';
+$lang['organization_postal_code_invalid'] = 'Poštové smerovacie číslo je neplatný.';
+$lang['organization_postal_code'] = 'Postal / PSČ';
+$lang['organization_region_invalid'] = 'Štát, provincia alebo región je neplatný.';
+$lang['organization_region'] = 'Štát, provincia alebo región';
+$lang['organization_street_address_invalid'] = 'Ulica je neplatná.';
+$lang['organization_street_address'] = 'Ulica';
+$lang['organization_unit_invalid'] = 'Jednotka je neplatný.';
+$lang['organization_unit'] = 'Jednotka';
