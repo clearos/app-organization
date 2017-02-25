408312b79309714845c61656864c7cfbacafa42f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:48:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Thông tin về tổ chức của bạn được sử dụng để tạo ra các chứng chỉ bảo mật và mặc định phần mềm khác.';
+$lang['organization_app_name'] = 'Cơ quan';
+$lang['organization_city'] = 'thành phố';
+$lang['organization_city_invalid'] = 'Thành phố là không hợp lệ.';
+$lang['organization_country'] = 'Quốc gia';
+$lang['organization_country_invalid'] = 'Nước không hợp lệ.';
+$lang['organization_organization_invalid'] = 'Tổ chức là không hợp lệ.';
+$lang['organization_organization'] = 'Cơ quan';
+$lang['organization_postal_code_invalid'] = 'mã bưu chính là không hợp lệ.';
+$lang['organization_postal_code'] = 'Bưu chính / Zip Code';
+$lang['organization_region_invalid'] = 'Nhà nước, tỉnh hoặc khu vực là không hợp lệ.';
+$lang['organization_region'] = 'Nhà nước, tỉnh hoặc khu vực';
+$lang['organization_street_address_invalid'] = 'địa chỉ đường phố là không hợp lệ.';
+$lang['organization_street_address'] = 'Địa chỉ';
+$lang['organization_unit_invalid'] = 'Đơn vị là không hợp lệ.';
+$lang['organization_unit'] = 'Đơn vị';
