e5c39ebd39a095703daf6b243b518dbb232d6c05
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:39:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Informace o vaší organizaci se používá k výrobě bezpečnostních certifikátů a další software výchozí nastavení.';
+$lang['organization_app_name'] = 'Organizace';
+$lang['organization_city'] = 'Město';
+$lang['organization_city_invalid'] = 'Město je neplatný.';
+$lang['organization_country'] = 'Země';
+$lang['organization_country_invalid'] = 'Země je neplatný.';
+$lang['organization_organization_invalid'] = 'Organizace je neplatný.';
+$lang['organization_organization'] = 'Organizace';
+$lang['organization_postal_code_invalid'] = 'Poštovní směrovací číslo je neplatný.';
+$lang['organization_postal_code'] = 'Postal / PSČ';
+$lang['organization_region_invalid'] = 'Stát, provincie nebo region je neplatný.';
+$lang['organization_region'] = 'Stát, provincie nebo region';
+$lang['organization_street_address_invalid'] = 'Ulice je neplatná.';
+$lang['organization_street_address'] = 'Adresa ulice';
+$lang['organization_unit_invalid'] = 'Jednotka je neplatný.';
+$lang['organization_unit'] = 'Jednotka';
