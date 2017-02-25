4d700bdc5ca579a2343022f462f3fd6c3f7b82e1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:41:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Bayani game da your kungiyar da aka amfani da su janye tsaro da takardun shaida da kuma sauran software Predefinicións.';
+$lang['organization_app_name'] = 'Organization';
+$lang['organization_city'] = 'City';
+$lang['organization_city_invalid'] = 'City ba daidai ba ne.';
+$lang['organization_country'] = 'Country';
+$lang['organization_country_invalid'] = 'Country ba daidai ba ne.';
+$lang['organization_organization_invalid'] = 'Organization ba daidai ba ne.';
+$lang['organization_organization'] = 'Organization';
+$lang['organization_postal_code_invalid'] = 'Lambar akwatin gidan waya ba daidai ba ne.';
+$lang['organization_postal_code'] = 'City / Zip Code';
+$lang['organization_region_invalid'] = 'State, lardin ko Region ba daidai ba ne.';
+$lang['organization_region'] = 'State, lardin ko Region';
+$lang['organization_street_address_invalid'] = 'Street address ba daidai ba ne.';
+$lang['organization_street_address'] = 'Street Address';
+$lang['organization_unit_invalid'] = 'Unit ba daidai ba ne.';
+$lang['organization_unit'] = 'Unit';
