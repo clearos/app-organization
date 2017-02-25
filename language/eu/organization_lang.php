c8f0051490822d051b734f820d37ebb739730608
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:38:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Zure erakundearen buruzko informazioa segurtasun ziurtagiriak eta beste software lehenetsiak sortzeko erabiltzen da.';
+$lang['organization_app_name'] = 'Erakundea';
+$lang['organization_city'] = 'hiria';
+$lang['organization_city_invalid'] = 'Hiria baliogabea da.';
+$lang['organization_country'] = 'Herriko';
+$lang['organization_country_invalid'] = 'Herriko baliogabea da.';
+$lang['organization_organization_invalid'] = 'Erakundeak baliogabea da.';
+$lang['organization_organization'] = 'Erakundea';
+$lang['organization_postal_code_invalid'] = 'Posta-kodea baliogabea da.';
+$lang['organization_postal_code'] = 'Postal / Zip Code';
+$lang['organization_region_invalid'] = 'Estatuko, Probintzia edo Region baliogabea da.';
+$lang['organization_region'] = 'Estatuko, Probintzia edo eskualdea';
+$lang['organization_street_address_invalid'] = 'Kalean helbidea baliogabea da.';
+$lang['organization_street_address'] = 'Helbidea';
+$lang['organization_unit_invalid'] = 'Unitate baliogabea da.';
+$lang['organization_unit'] = 'unitatea';
