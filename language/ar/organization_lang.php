5c45c2165ef63f322c9161c041b2a1b93f968c4c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:38:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'يتم استخدام المعلومات حول مؤسستك لتوليد شهادات الأمن وغيرها من التخلف البرمجيات.';
+$lang['organization_app_name'] = 'منظمة';
+$lang['organization_city'] = 'مدينة';
+$lang['organization_city_invalid'] = 'مدينة غير صحيحة.';
+$lang['organization_country'] = 'بلد';
+$lang['organization_country_invalid'] = 'بلد غير صالح.';
+$lang['organization_organization_invalid'] = 'منظمة غير صالحة.';
+$lang['organization_organization'] = 'منظمة';
+$lang['organization_postal_code_invalid'] = 'الرمز البريدي غير صالح.';
+$lang['organization_postal_code'] = 'البريدي / الرمز البريدي';
+$lang['organization_region_invalid'] = 'الدولة، مقاطعة أو منطقة غير صالحة.';
+$lang['organization_region'] = 'الدولة، مقاطعة أو منطقة';
+$lang['organization_street_address_invalid'] = 'عنوان الشارع غير صالح.';
+$lang['organization_street_address'] = 'عنوان الشارع';
+$lang['organization_unit_invalid'] = 'وحدة غير صالحة.';
+$lang['organization_unit'] = 'وحدة';
