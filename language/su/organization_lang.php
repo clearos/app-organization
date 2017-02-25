62a3ae996f3397f0e85f321839942bf3c2b96ade
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:47:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Informasi ngeunaan organisasi Anjeun dipaké pikeun ngahasilkeun sertipikat kaamanan jeung ingkar software lianna.';
+$lang['organization_app_name'] = 'organisasi';
+$lang['organization_city'] = 'kota';
+$lang['organization_city_invalid'] = 'City téh sah.';
+$lang['organization_country'] = 'nagara';
+$lang['organization_country_invalid'] = 'Nagara anu sah.';
+$lang['organization_organization_invalid'] = 'Organisasi anu sah.';
+$lang['organization_organization'] = 'organisasi';
+$lang['organization_postal_code_invalid'] = 'kode pos téh sah.';
+$lang['organization_postal_code'] = 'Pos / pos Code';
+$lang['organization_region_invalid'] = 'State, Province atawa Region téh sah.';
+$lang['organization_region'] = 'State, Province atawa Region';
+$lang['organization_street_address_invalid'] = 'alamat jalan anu sah.';
+$lang['organization_street_address'] = 'Alamat jalan';
+$lang['organization_unit_invalid'] = 'Unit téh sah.';
+$lang['organization_unit'] = 'unit';
