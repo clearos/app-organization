edf6abcf55c0ef1b9691bbf1d99da73e3c79dcff
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:41:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Tá eolas maidir leis an eagraíocht a úsáidtear chun deimhnithe slándála agus mainneachtainí bogearraí eile a chomhlíonadh.';
+$lang['organization_app_name'] = 'Eagraíocht';
+$lang['organization_city'] = 'Cathair';
+$lang['organization_city_invalid'] = 'Is Cathair neamhbhailí.';
+$lang['organization_country'] = 'Country';
+$lang['organization_country_invalid'] = 'Is Country neamhbhailí.';
+$lang['organization_organization_invalid'] = 'Is eagraíocht neamhbhailí.';
+$lang['organization_organization'] = 'Eagraíocht';
+$lang['organization_postal_code_invalid'] = 'Is cód poist neamhbhailí.';
+$lang['organization_postal_code'] = 'Poist / Cód Zip';
+$lang['organization_region_invalid'] = 'Stáit, tá Riarachán Réigiún neamhbhailí.';
+$lang['organization_region'] = 'Stáit, Riarachán Réigiún';
+$lang['organization_street_address_invalid'] = 'Is seoladh Street neamhbhailí.';
+$lang['organization_street_address'] = 'Ainm na sráide';
+$lang['organization_unit_invalid'] = 'Tá an tAonad neamhbhailí.';
+$lang['organization_unit'] = 'Aonad';
