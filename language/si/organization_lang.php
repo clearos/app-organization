6c09655722fd491934d073d31ea15c0ea1e163f0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:46:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'ඔබගේ සංවිධානය පිළිබඳ තොරතුරු ආරක්ෂක සහතික සහ වෙනත් මෘදුකාංග පෙරනිමි උත්පාදනය කිරීමට භාවිතා කරයි.';
+$lang['organization_app_name'] = 'ආයතනය';
+$lang['organization_city'] = 'නගරය';
+$lang['organization_city_invalid'] = 'නගරය වලංගු නැත.';
+$lang['organization_country'] = 'රට';
+$lang['organization_country_invalid'] = 'රටේ වලංගු නැත.';
+$lang['organization_organization_invalid'] = 'සංවිධානය වලංගු නැත.';
+$lang['organization_organization'] = 'ආයතනය';
+$lang['organization_postal_code_invalid'] = 'තැපැල් කේතය වලංගු නැත.';
+$lang['organization_postal_code'] = 'තැපැල් කේතය / නගර අංකය';
+$lang['organization_region_invalid'] = 'රාජ්ය, පළාත් හෝ කලාපය වලංගු නැත.';
+$lang['organization_region'] = 'රාජ්ය, පළාත් හෝ කලාපය';
+$lang['organization_street_address_invalid'] = 'වීදි ලිපිනය වලංගු නැත.';
+$lang['organization_street_address'] = 'වීථි ලිපිනය';
+$lang['organization_unit_invalid'] = 'ඒකකය වලංගු නැත.';
+$lang['organization_unit'] = 'ඒකකය';
