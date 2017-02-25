0e064622f674be3977a10f0c8fb32e6653c8b148
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:47:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Маълумот дар бораи нигоҳдории ташкилоти худ истифода бурда мешавад барои тавлиди шаҳодатномаҳои амниятӣ ва ба дигар пешфарзро нармафзор.';
+$lang['organization_app_name'] = 'ташкилот';
+$lang['organization_city'] = 'шаҳр';
+$lang['organization_city_invalid'] = 'Сити нодуруст аст.';
+$lang['organization_country'] = 'кишвар';
+$lang['organization_country_invalid'] = 'Кишвар нодуруст аст.';
+$lang['organization_organization_invalid'] = 'Ташкилоти нодуруст аст.';
+$lang['organization_organization'] = 'ташкилот';
+$lang['organization_postal_code_invalid'] = 'Рамзи почта нодуруст аст.';
+$lang['organization_postal_code'] = 'Почта / Кодекси Zip';
+$lang['organization_region_invalid'] = 'Давлатӣ, ноҳия ё вилоят нодуруст аст.';
+$lang['organization_region'] = 'Давлатӣ, ноҳия ё вилоят';
+$lang['organization_street_address_invalid'] = 'Суроғаи кӯчаи нодуруст аст.';
+$lang['organization_street_address'] = 'Нишона';
+$lang['organization_unit_invalid'] = 'Шўъбаи нодуруст аст.';
+$lang['organization_unit'] = 'Шӯъбаи';
