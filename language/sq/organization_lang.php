63ea18e8ea0491096714aac1185aab50004bf259
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:38:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Informacion në lidhje me organizatën tuaj është përdorur për të gjeneruar certifikata sigurie dhe standarte të tjera të software.';
+$lang['organization_app_name'] = 'organizatë';
+$lang['organization_city'] = 'qytet';
+$lang['organization_city_invalid'] = 'Qyteti është i pavlefshëm.';
+$lang['organization_country'] = 'vend';
+$lang['organization_country_invalid'] = 'Vendi është i pavlefshëm.';
+$lang['organization_organization_invalid'] = 'Organizata është e pavlefshme.';
+$lang['organization_organization'] = 'organizatë';
+$lang['organization_postal_code_invalid'] = 'Kodi postar është i pavlefshëm.';
+$lang['organization_postal_code'] = 'Postal / Kodi';
+$lang['organization_region_invalid'] = 'State, Province ose rajoni është e pavlefshme.';
+$lang['organization_region'] = 'State, Province ose rajoni';
+$lang['organization_street_address_invalid'] = 'Adresa Rruga është e pavlefshme.';
+$lang['organization_street_address'] = 'Adresa e rrugës';
+$lang['organization_unit_invalid'] = 'Njësia është e pavlefshme.';
+$lang['organization_unit'] = 'njësi';
