7568e3ea64a4f01a957b4a2f6d2826e6457792c8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:40:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Organisaation tiedot käytetään tuottamaan varmenteita ja muita ohjelmia oletusarvot.';
+$lang['organization_app_name'] = 'organisaatio';
+$lang['organization_city'] = 'Kaupunki';
+$lang['organization_city_invalid'] = 'Kaupunki ei kelpaa.';
+$lang['organization_country'] = 'Maa';
+$lang['organization_country_invalid'] = 'Maa ei kelpaa.';
+$lang['organization_organization_invalid'] = 'Organisaatio on virheellinen.';
+$lang['organization_organization'] = 'organisaatio';
+$lang['organization_postal_code_invalid'] = 'Postinumero on virheellinen.';
+$lang['organization_postal_code'] = 'Postal / Postinumero';
+$lang['organization_region_invalid'] = 'Osavaltio, provinssi tai alue on virheellinen.';
+$lang['organization_region'] = 'Osavaltio, provinssi tai -alue';
+$lang['organization_street_address_invalid'] = 'Katuosoite on virheellinen.';
+$lang['organization_street_address'] = 'Katuosoite';
+$lang['organization_unit_invalid'] = 'Yksikkö on virheellinen.';
+$lang['organization_unit'] = 'yksikkö';
