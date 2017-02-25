0f819faed12a07818978898f92c47cd242e302fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:40:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Πληροφορίες για τον οργανισμό σας χρησιμοποιείται για να παράγει τα πιστοποιητικά ασφαλείας και άλλες προεπιλογές του λογισμικού.';
+$lang['organization_app_name'] = 'Οργάνωση';
+$lang['organization_city'] = 'Πόλη';
+$lang['organization_city_invalid'] = 'City είναι άκυρη.';
+$lang['organization_country'] = 'Χώρα';
+$lang['organization_country_invalid'] = 'Χώρα δεν είναι έγκυρο.';
+$lang['organization_organization_invalid'] = 'Οργάνωση δεν είναι έγκυρο.';
+$lang['organization_organization'] = 'Οργάνωση';
+$lang['organization_postal_code_invalid'] = 'Ταχυδρομικός κώδικας δεν είναι έγκυρος.';
+$lang['organization_postal_code'] = 'Ταχυδρομική / Τ.Κ.';
+$lang['organization_region_invalid'] = 'Κράτος, επαρχία ή περιοχή δεν είναι έγκυρο.';
+$lang['organization_region'] = 'Κράτος, επαρχία ή περιφέρεια';
+$lang['organization_street_address_invalid'] = 'Οδός δεν είναι έγκυρη.';
+$lang['organization_street_address'] = 'Διεύθυνση';
+$lang['organization_unit_invalid'] = 'Μονάδα δεν είναι έγκυρο.';
+$lang['organization_unit'] = 'Μονάδα';
