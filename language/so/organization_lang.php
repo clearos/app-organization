d74dd89769043a08bfa1020d5a216b51de32cc29
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:46:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Macluumaadka ku saabsan ururka waxaa loo isticmaalaa in dhalin shahaadooyin ammaanka iyo la bixin kari kale software.';
+$lang['organization_app_name'] = 'Organization';
+$lang['organization_city'] = 'City';
+$lang['organization_city_invalid'] = 'City waa baadil.';
+$lang['organization_country'] = 'Country';
+$lang['organization_country_invalid'] = 'Country waa baadil.';
+$lang['organization_organization_invalid'] = 'Ururka waa baadil.';
+$lang['organization_organization'] = 'Organization';
+$lang['organization_postal_code_invalid'] = 'code Boostada waa baadil.';
+$lang['organization_postal_code'] = 'Boostada / Zip Code';
+$lang['organization_region_invalid'] = 'State, Gobolka ama gobolka waa baadil.';
+$lang['organization_region'] = 'State, Gobolka ama gobolka';
+$lang['organization_street_address_invalid'] = 'cinwaanka Street waa baadil.';
+$lang['organization_street_address'] = 'Cinwaanka Street';
+$lang['organization_unit_invalid'] = 'Unit waa baadil.';
+$lang['organization_unit'] = 'Unit';
