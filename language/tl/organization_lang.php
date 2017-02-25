ad08bea1939a09b036840c832b462d344504d9f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:47:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'Impormasyon tungkol sa iyong organisasyon ay ginagamit upang bumuo ng mga sertipiko ng seguridad at iba pang mga software default.';
+$lang['organization_app_name'] = 'samahan';
+$lang['organization_city'] = 'lungsod';
+$lang['organization_city_invalid'] = 'City ay hindi wasto.';
+$lang['organization_country'] = 'bansa';
+$lang['organization_country_invalid'] = 'Bansa ay hindi wasto.';
+$lang['organization_organization_invalid'] = 'Organization ay hindi wasto.';
+$lang['organization_organization'] = 'samahan';
+$lang['organization_postal_code_invalid'] = 'Postal code ay hindi wasto.';
+$lang['organization_postal_code'] = 'Postal / ZIP Code';
+$lang['organization_region_invalid'] = 'Estado, Lalawigan o Rehiyon ay hindi wasto.';
+$lang['organization_region'] = 'Estado, Lalawigan o Rehiyon';
+$lang['organization_street_address_invalid'] = 'Street address ay hindi wasto.';
+$lang['organization_street_address'] = 'Street Address';
+$lang['organization_unit_invalid'] = 'Unit ay hindi wasto.';
+$lang['organization_unit'] = 'yunit';
