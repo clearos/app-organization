d2491f734594fd3d8c9060e1c363a1c4041bb3e1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:48:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = 'אינפֿאָרמאַציע וועגן דיין אָרגאַניזאַציע איז געניצט צו דזשענערייט זיכערהייַט certificates און אנדערע ווייכווארג דעפאַולץ.';
+$lang['organization_app_name'] = 'אָרגאַניזירונג';
+$lang['organization_city'] = 'שטאָט';
+$lang['organization_city_invalid'] = 'סיטי איז פאַרקריפּלט.';
+$lang['organization_country'] = 'לאַנד';
+$lang['organization_country_invalid'] = 'לאַנד איז פאַרקריפּלט.';
+$lang['organization_organization_invalid'] = 'ארגאניזאציע איז פאַרקריפּלט.';
+$lang['organization_organization'] = 'אָרגאַניזירונג';
+$lang['organization_postal_code_invalid'] = 'פּאָסטאַל קאָד איז פאַרקריפּלט.';
+$lang['organization_postal_code'] = 'פּאָסטאַל / זיפּ קאָוד';
+$lang['organization_region_invalid'] = 'שטאַט, פּראָווינץ אָדער קאנט איז פאַרקריפּלט.';
+$lang['organization_region'] = 'שטאַט, פּראָווינץ אָדער געגנט';
+$lang['organization_street_address_invalid'] = 'סטריט אַדרעס איז פאַרקריפּלט.';
+$lang['organization_street_address'] = 'אדרעס';
+$lang['organization_unit_invalid'] = 'יוניט איז פאַרקריפּלט.';
+$lang['organization_unit'] = 'אַפּאַראַט';
