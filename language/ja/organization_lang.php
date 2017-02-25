39411d74b8d358ab8db0e7832164ca0bd879ff01
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:42:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/organization_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['organization_app_description'] = '組織に関する情報は、セキュリティ証明書、およびその他のソフトウェアのデフォルト設定を生成するために使用されます。';
+$lang['organization_app_name'] = '組織';
+$lang['organization_city'] = 'シティ';
+$lang['organization_city_invalid'] = '市は無効です。';
+$lang['organization_country'] = '国';
+$lang['organization_country_invalid'] = '国が無効です。';
+$lang['organization_organization_invalid'] = '組織が無効です。';
+$lang['organization_organization'] = '組織';
+$lang['organization_postal_code_invalid'] = '郵便番号が無効です。';
+$lang['organization_postal_code'] = '郵便番号';
+$lang['organization_region_invalid'] = '州は、州または地域が無効です。';
+$lang['organization_region'] = '州、州または地域';
+$lang['organization_street_address_invalid'] = 'ストリートアドレスが無効です。';
+$lang['organization_street_address'] = '住所';
+$lang['organization_unit_invalid'] = 'ユニットは無効です。';
+$lang['organization_unit'] = '単位';
